9cbe180cb0ef8e5664eb4dcd62f383bfc0b82a1f
Abdul-Jobber-2407 <arjobbar2019@gmail.com>
2025-02-19 10:09:38 +0600

    first commit

---
+++ 1-1--how-does-internet-work--HTTPs-and-API.js
@@ -0,0 +1,22 @@
+/**
+  ****What is Internet ? Real Life Example :
+  The internet is  a worldwide system of computer networks.And A network of networks in 
+  which users at any one computer can , if they have permission , get information from any other computer. 
+ 
+ **** What is IP address ?
+  An IP address is a unique address that identifies a device on the internet or a local network.
+ 
+  *** Steps of DNS :
+  1. When a user enters a domain name in a browser, the server will search through a global server network that makes up the Domain Name System(DNS).
+
+  2.The request is made for one DNS server and if the address is notfound there , it moves to the others.
+  3.The DNS server that has information about the IP address will return it to the browser.
+  4.The browser will  request data about the site from the domains hosting server. once  the hosting server sends the data back, the web browser will convert it into a web page.  
+  ***How DNS Works ?
+
+  *** HOW THE INTERNET WORKS ? 
+   The data is split into packets that travel through multiplenetwork paths .  
+  
+  
+ 
+ */
---
+++ 1-2-api.js
@@ -0,0 +1,3 @@
+// const user = { id:1, name: 'Amir Khan', job: 'Actor'}
+// console.log(user);
+
